a3e989a5dca8232d8f40a02eca1d6f275ef73185
IrinaTurcas <irina_turcas@outlook.com>
2022-03-07 15:36:26 +0200

    3D donut

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, useState } from "react"
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+import { EffectComposer, Outline } from '@react-three/postprocessing'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Model from './Donut.js'
+import './App.css'
+
+function intersectionsFilter(intersections) {
+  return intersections?.length ? [intersections[0]] : intersections
 }
 
-export default App;
+export default function App() {
+  const [active, setActive] = useState(null)
+  const selected = active ? [active] : undefined
+
+  return (
+    <>
+      <Canvas 
+          style={{ background: "transparent", height: "100%", width: "100%" }} 
+          camera={{ fov: 70, position: [0.1, 0.1, 0.1] }}
+          raycaster={{ filter: intersectionsFilter }}
+          >
+        <ambientLight intensity={1}/>
+        <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+        <Suspense fallback={null}>
+          <Model setActive={setActive}/>
+        </Suspense>
+        <OrbitControls enableZoom={false}/>
+        <EffectComposer multisampling={8} autoClear={false}>
+          <Outline blur selection={selected} visibleEdgeColor="white" edgeStrength={10} width={1000} />
+        </EffectComposer>
+      </Canvas>
+      <div className="overlay">
+        <div className="title">
+          <span className="stroked text">Donut&nbsp;</span>
+          <span className="donut" onClick={() => console.log("Gogoasa")}>🍩</span>
+        </div>
+      </div>
+    </>
+  );
+}
